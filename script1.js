b80a3c390ee2ec212fa17240e307a77fc7cf56d5
Vanshita <vanshita010@gmail.com>
2021-08-16 15:54:32 +0530

    Initial commit

---
+++ script1.js
@@ -0,0 +1,10 @@
+function hide() {
+    let btn = document.getElementById('call');
+    let num = document.getElementById('number');
+    if (number.style.display != 'block') {
+        number.style.display = 'block';
+    }
+    else {
+        number.style.display = 'none';
+    }
+}
